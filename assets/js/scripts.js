02474a69f73629d932bf6d3ebdfd797728d2e836
David Xiao <davidxiao93@gmail.com>
2016-06-19 12:42:11 -0700

    fancy loading image

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -119,7 +119,7 @@ function init() {
 	var imgDefer = document.getElementsByTagName('img');
 	for (var i=0; i<imgDefer.length; i++) {
 		if(imgDefer[i].getAttribute('data-src')) {
-				imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
+			imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
 		} 
 	} 
 	var vidDefer = document.getElementsByTagName('iframe');
